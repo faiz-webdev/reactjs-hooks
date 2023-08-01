79dc2cc0583770ca77e87c48a2b9877e7166b8a6
faiz-webdev <faijuddin@tothenew.com>
2023-08-01 15:43:18 +0530

    1. create employee and employee-list component

---
+++ src/components/Employee.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Employee() {
+  return (
+    <div>
+      <ul>
+        <li>Employee1</li>
+        <li>Employee2</li>
+        <li>Employee3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Employee;
---
+++ src/components/EmployeeList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Employee from './Employee'
+
+function EmployeeList() {
+  return (
+    <div><Employee /></div>
+  )
+}
+
+export default EmployeeList
