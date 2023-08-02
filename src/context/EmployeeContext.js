4c1af51bce6c79bd1a900196c7052758b909c8ac
faiz-webdev <faijuddin@tothenew.com>
2023-08-02 13:37:52 +0530

    13. localstorage

--- src/context/EmployeeContext.js
+++ src/context/EmployeeContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export const EmployeeContext = createContext();
@@ -41,6 +41,14 @@ const EmployeeContextProvider = (props) => {
       phone: "(480) 631-2097",
     },
   ]);
+
+  useEffect(()=>{
+    setEmployees(JSON.parse(localStorage.getItem('employees')))
+  }, [])
+
+  useEffect(()=>{
+    localStorage.setItem('employees', JSON.stringify(employees))
+  })
 
   const sortedEmployees = employees.sort((a, b) => (a.name < b.name ? -1 : 1));
 
